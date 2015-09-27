9249bba27fbaffaa71d9c922d6bcce0fecb2c800
andrew@dirksen.com <andrew@dirksen.com>
2015-09-27 14:27:02 -0700

    A test.

--- Song.hs
+++ Song.hs
@@ -4,7 +4,7 @@ module Song where
 import Gensounds
 import Data.Fixed (mod')
 
-songlen = 8.0
+songlen = 32.0
 
 pi2 song = speed (pi * 2) song
 
@@ -12,12 +12,24 @@ volume v song = (*v) . song
 
 speed s f = f . (* s)
 
+square t = if (even . floor) t then 1 else (-1)
+
+shift offset song = song . (+offset)
+
 saw t = speed 600 m t
   where
     m ti = mod' ti 2.0 - 1.0
 
-ramp t =  speed (t * 600 * pi * 2) sin t
+compo t = sum $ map ($ t) sines
+  where
+    sines = map sine coefficients
+    coefficients = [600]
+    sine freq = speed freq $ pi2 sin
 
-sine = speed 600 $ pi2 sin
+composquared t = sample ** (speed 10 square t / 2 + 2.5) * doneg
+  where
+    sample = speed s compo t
+    doneg = if sample < 0 then (-1) else 1
+    s = mod' (fromIntegral (speed t floor t)) 4
 
-main = gen "out.wav" songlen sine
+main = gen "out.wav" songlen $ shift (-4) composquared
