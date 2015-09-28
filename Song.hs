cc1ff27c81a0b26690ea2bae5e5bb13f10bf6f38
andrew@dirksen.com <andrew@dirksen.com>
2015-09-27 20:03:06 -0700

    Experiment with playing a list of notes.

--- Song.hs
+++ Song.hs
@@ -3,5 +3,17 @@ module Song where
 import Gensounds
 import GensoundsUtil
 
-song = (attack 1 2) . (sustain 6 7) $ pleasingtoggle
+note n = sine . (2.0 ** n *)
+
+play n = (attack 0 0.5) . (sustain 0.5 1) $ note n
+
+playnotes [] = n
+  where n x = 0
+playnotes (x:xs) = both (play x) $ (playnotes xs) . (subtract 0.5)
+
+vibrato freq f t = f t * mute
+  where mute = sine (t * freq) * 0.5 + 0.5
+
+song = vibrato 8 $ playnotes $ take 16 [7,7.25..]
+
 main = gen "out.wav" 8.0 song
