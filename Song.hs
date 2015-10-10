6f8f95caab8b9ce9b53b066373e5a1fdbdf258aa
andrew@dirksen.com <andrew@dirksen.com>
2015-10-10 14:30:17 -0700

    Working on a scale.

--- Song.hs
+++ Song.hs
@@ -1,15 +1,26 @@
 module Song where
 
 import Gensounds
-import Conglomeration
---import GensoundsUtil
+import System.Random
 
---song t = sin (t*600*pi*2+(sin (t*pi*2*16))*100)
+rg = map (`mod` possibilities) $ rl $ next generator
+  where
+    rl (v,g) = v : (rl $ next g)
+    generator = mkStdGen 0
 
-song = deetoodeetoo
+song t = sin . freq $ t
+  where
+    freq = (2*pi*10*t*)
 
-main = gen "out.wav" 16.0 song
---main = putStr $ show $ fromCoefficients [1] (pi / 20)
+--main = gen "out.wav" 16.0 song
+main = putStr $ show $ take 1024 $ map (note . fromIntegral) rg
 
---12341231231234123123
---11110000001111000222
+note n = freq
+  where
+    freq = 16.0 * 2.0 ** (n / 16)
+
+bottom = 16
+top = 2 ** topexpo
+topexpo = 14
+resolution = 2**4
+possibilities = topexpo*resolution
